2ba457d278edec72acd84d1b6eb5f6e7e734f541
Karanjit Gill <ksg207@live.com>
2017-04-25 03:27:38 +0530

    :sparkles: partition.cpp : Added unPropagate function

--- CST/partition.cpp
+++ CST/partition.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
+#include <vector>
 
-int 
+#define HEAD_PARENT_ID -1
+/*
+* @params: CST node array, node id(whose subtree contains the child node) and the child node id
+* @returns: edge-cut cost
+*/
+void unPropagate (vector<Node> CST, int nodeId, int childNodeId)
+{
+  int parentPos = -1, childPos = -1;
+
+  while(nodeId != HEAD_PARENT_ID)
+  {
+    for(int i = 0; i < CST.size(); i++)
+    {
+      if(CST[i].userId == nodeId)
+        parentPos = i;
+      else if(CST[i].userId == childNodeId)
+        childPos = i;
+
+      if(parentPos >= 0 && childPos >= 0)
+        break;
+    }
+    CST[parentPos].nodeWeight -= CST[childPos].nodeWeight;
+    nodeId = CST[parentPos].parentUserId;
+  }
+}
